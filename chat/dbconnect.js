e2c2735ccf5711eb12beff97f43ee23e8913589c
Roman Solovev <33293285+rsolovev@users.noreply.github.com>
2019-10-30 12:02:42 +0300

    bd connect fix

--- chat/dbconnect.js
+++ chat/dbconnect.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 mongoose.Promise = require("bluebird");
 
-const url = "mongodb://localhost:27017/chat";
+const url = "mongodb://localhost:27017,node1:27107,node2:271017,node3:27017/chat";
 
 const connect = mongoose.connect(url, { useNewUrlParser: true });
 
